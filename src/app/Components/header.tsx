ca2b85e4c3a740942a72d2018aa347959a4f60de
fimbulwinters <dakin.92@outlook.com>
2023-01-15 09:12:22 +0000

    Add nav bar

---
+++ src/app/Components/header.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header className=" bg-slate-700 sticky top-0  p-5 flex  justify-between max-w-7xl mx-auto z-30">
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center"
+      >
+        <div>
+          {/* socials */}
+          <SocialIcon
+            url="https://github.com/FimbulWinters"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/alex-winters-083337209"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ x: 500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className=" flex flex-row items-center"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Contact me
+        </p>
+      </motion.div>
+    </header>
+  );
+}
+
+export default Header;
